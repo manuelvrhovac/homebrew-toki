5e59d9ffd19e5e0f510ad0eb04d542c021dd6947
Manuel Vrhovac <manuel.vrhovac@infinum.com>
2025-08-30 18:54:29 +0200

    Update toki.rb

--- Formula/toki.rb
+++ Formula/toki.rb
@@ -2,7 +2,7 @@ class Toki < Formula
   desc "Generate Xcode asset catalogs and Swift references from Figma tokens"
   homepage "https://github.com/manuelvrhovac/toki"
   url "https://github.com/manuelvrhovac/toki/releases/download/v0.8.0/toki-v0.8.0-macos.zip"
-  sha256 "a5ea319649c4bcc580ef7f2b43e7820093b4d2e0d2d0f28c44b6c31e158c083d"
+  sha256 "59f40b0c850e75fcc921237186ad424318215bb968b94834881be490581cf7ed"
   license "Apache-2.0"
 
   def install
